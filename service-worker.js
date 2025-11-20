8e9e33eee4990915cd80521fd4ff1b5ecec5fb86
Maw3sio <lorenzo.marinello@hotmail.com>
2025-11-20 22:47:26 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,16 @@
-self.addEventListener("install", e => {
+const CACHE = 'sxrate-v1';
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './logo.png',
+      './icon-512.png'
+    ]))
+  );
   self.skipWaiting();
 });
-
-self.addEventListener("fetch", e => {});
+self.addEventListener('fetch', evt => {
+  evt.respondWith(caches.match(evt.request).then(r => r || fetch(evt.request)));
+});
